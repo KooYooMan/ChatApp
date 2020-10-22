1fa6fb69be09ab02af64a837321295b8d6853f96
hoangvuduyanh33 <emanhnhi124@gmail.com>
2020-10-22 14:53:17 +0700

    Intergrate firebase with app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:ChatApp/src/screens/auth_screens/welcome_screen.dart';
 
-void main() => runApp(MyApp());
-
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(MyApp());
+}
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
