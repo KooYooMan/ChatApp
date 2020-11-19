9c5548c5a6c595dc78d37d57bf16d40805f21b1f
hoangvuduyanh33 <emanhnhi124@gmail.com>
2020-11-19 23:25:58 +0700

    Initialize authentication screens

---
+++ lib/src/screens/auth_screens/styles/font_styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+TextStyle simpleTextFieldStyle(){
+  return TextStyle(
+      color: Colors.white,
+      fontSize: 16
+  );
+}
+
+TextStyle simpleMediumTextStyle(){
+  return TextStyle(
+      color: Colors.white,
+      fontSize: 17
+  );
+}
---
+++ lib/src/screens/auth_screens/widgets/auth_app_bar.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Widget authAppBar(BuildContext context) {
+  return AppBar(
+    title: Image.asset("assets/images/logo.png"),
+  );
+}
